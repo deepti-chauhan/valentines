86557dd5a3eaf755e3f21e9b996be622f8a564ac
root <root@DESKTOP-MKAOB9U>
2024-02-12 18:59:04 +0530

    style : add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import "./App.css";
 function App() {
   const [isYes, setIsYes] = useState<boolean>(false);
   const [padding, setPadding] = useState<number>(10);
+  const [disabled, setDisabled] = useState<boolean>(false);
 
   const yesButtonStyle: React.CSSProperties = {
     padding: `${padding}px`,
@@ -16,10 +17,12 @@ function App() {
   const noButtonStyle: React.CSSProperties = {
     padding: "10px",
     backgroundColor: "red",
+    cursor: disabled ? "not-allowed" : "pointer",
   };
   const handleYesClick = () => {
     setPadding(10);
     setIsYes(true);
+    setDisabled(true);
   };
   const handleNoClick = () => {
     setPadding((padding) => padding + 10);
@@ -28,20 +31,30 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Dear Boyfriend</h1>
-      {!isYes ? <h1>Will you be my valentines?</h1> : <h1>Yaayy!!</h1>}
-
-      {isYes ? (
-        <img src={happyGif} width={400} height={300} />
-      ) : (
-        <img src={requestGif} width={400} height={300} />
-      )}
+      <h1>Dear Boyfriend 💖</h1>
+      {!isYes ? <h1>💐Will you be my valentines?💐</h1> : <h1>Yaayy!!</h1>}
+      <div className="vector-shadow">
+        {isYes ? (
+          <img src={happyGif} width={400} height={300} className="mocha-img" />
+        ) : (
+          <img
+            src={requestGif}
+            width={400}
+            height={300}
+            className="mocha-img"
+          />
+        )}
+      </div>
 
       <div>
         <button onClick={handleYesClick} style={yesButtonStyle} className="">
           Yes
         </button>
-        <button onClick={handleNoClick} style={noButtonStyle}>
+        <button
+          onClick={handleNoClick}
+          style={noButtonStyle}
+          disabled={disabled}
+        >
           No
         </button>
       </div>
